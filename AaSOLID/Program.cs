5ea60380ad8b89ad40a33e3d48d99fd70b329003
DESKTOP-JLR2A67\Lenovo <demon5513@list.ru>
2024-08-09 03:36:12 +0300

    Старт

---
+++ AaSOLID/Program.cs
@@ -0,0 +1,60 @@
+﻿using AaSOLID.Setting;
+
+int min = default(int);
+int max = 20;
+int numberNumberAttempt = 10;
+int? userNumber = null;
+bool result = false;
+
+if(args.Length >= 3)
+{
+    int.TryParse(args[0].ToString(), out min);
+    int.TryParse(args[1].ToString(), out max);
+    int.TryParse(args[2].ToString(), out numberNumberAttempt);
+}
+
+var setting = new Setting(min, max, numberNumberAttempt);
+
+var random = new Random();
+var target = random.Next(setting.Min, setting.Max);
+Console.WriteLine("Игра угадай число привествует вас");
+Console.WriteLine("Введите число");
+for (int i = 0; i < setting.NumberAttempts; i++)
+{
+    userNumber = null;
+    while (userNumber == null)
+    {
+        try
+        {
+            userNumber = int.Parse(Console.ReadLine());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ой вы ввели что-то не то");
+        }
+    }
+    if (userNumber == target)
+    {
+        Console.WriteLine("Ура вы выйграли");
+        result = true;
+        break;
+    }
+    else if(userNumber < target)
+    {
+        Console.WriteLine("Увы но не повезло ваше число меньше");
+    }
+    else if (userNumber > target)
+    {
+        Console.WriteLine("Увы но не повезло ваше число больше");
+    }
+
+
+}
+
+if (!result)
+{
+    Console.WriteLine("Вы проиграли");
+}
+
+
+
---
+++ AaSOLID/SOLID/Setting.cs
@@ -0,0 +1,23 @@
+﻿namespace AaSOLID.Setting
+{
+    internal class Setting
+    {
+        #region private field
+ 
+        #endregion
+
+        #region constructor
+        public Setting(int min, int max, int numberAttempts)
+        {
+            Min = min;
+            Max = max;
+            NumberAttempts = numberAttempts;
+        }
+
+        public int Min { get; private set; }
+        public int Max { get; private set; }
+        public int NumberAttempts { get; private set; }
+        #endregion
+
+    }
+}
